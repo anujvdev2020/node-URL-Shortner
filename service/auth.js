b4f1e4b100426defac7ec39ab07adaf0959f3840
Anuj Verma <anuj.v@protonshub.com>
2024-04-23 00:54:13 +0530

    added JWT token auth

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,23 @@
-const sessionIdToUserMap=new Map();
+const jwt = require("jsonwebtoken")
+const secret="Anuj@1998"
+function setUser(user){
+    
+    return jwt.sign({
+        id:user._id,
+        email:user.email
+    },secret)
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user)
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+    if(!token) return null;
+    try{
+        return jwt.verify(token,secret)
+    }
+   catch(error){
+
+    console.log("errr",error)
+   }
 }
 
 
